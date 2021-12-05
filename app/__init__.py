d7838961913f451e67f920280ff81860dddb613a
bfilipov <filipov.bogomil@gmail.com>
2021-12-06 01:09:48 +0200

    remove logging from app

--- app/__init__.py
+++ app/__init__.py
@@ -42,16 +42,16 @@ def create_app(config_class=Config):
     app.redis = Redis.from_url(app.config['REDIS_URL'])
     app.task_queue = rq.Queue('warehouse-ame-tasks', connection=app.redis)
 
-    if not app.debug:
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/warehouse-game.log',
-                                           maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s '
-            '[in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+    # if not app.debug:
+    #     if not os.path.exists('logs'):
+    #         os.mkdir('logs')
+    #     file_handler = RotatingFileHandler('logs/warehouse-game.log',
+    #                                        maxBytes=10240, backupCount=10)
+    #     file_handler.setFormatter(logging.Formatter(
+    #         '%(asctime)s %(levelname)s: %(message)s '
+    #         '[in %(pathname)s:%(lineno)d]'))
+    #     file_handler.setLevel(logging.INFO)
+    #     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Warehouse-game startup')
